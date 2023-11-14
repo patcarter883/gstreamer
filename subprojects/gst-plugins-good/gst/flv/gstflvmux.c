e6f58dbf68495e5a293eedff4433a037fdb61c7a
Pat Carter <patcarter@agripath.com.au>
2023-11-14 15:08:40 +1100

    Don't send flv h264 codec data when using an extended header

--- subprojects/gst-plugins-good/gst/flv/gstflvmux.c
+++ subprojects/gst-plugins-good/gst/flv/gstflvmux.c
@@ -1646,7 +1646,8 @@ gst_flv_mux_prepare_src_caps (GstFlvMux * mux, GstBuffer ** header_buf,
     GstFlvMuxPad *pad = l->data;
 
     /* Get H.264 and AAC codec data, if present */
-    if (pad && mux->video_pad == pad && pad->codec == 7) {
+    if (pad && mux->video_pad == pad && pad->codec == 7
+        && pad->is_ex_header == FALSE) {
       if (pad->codec_data == NULL)
         GST_WARNING_OBJECT (mux, "Codec data for video stream not found, "
             "output might not be playable");
@@ -1934,6 +1935,11 @@ gst_flv_mux_write_buffer (GstFlvMux * mux, GstFlvMuxPad * pad,
     while (total_consumed < map.size) {
       res = gst_av1_parser_identify_one_obu (mux->parser,
           map.data + total_consumed, map.size, &obu, &consumed);
+      if (res == GST_AV1_PARSER_DROP) {
+        total_consumed += consumed;
+        continue;
+      }
+
       if (res != GST_AV1_PARSER_OK) {
         ret = GST_FLOW_ERROR;
         GST_ERROR_OBJECT (mux, "can not parse input buffer");
@@ -2352,7 +2358,6 @@ gst_flv_mux_aggregate (GstAggregator * aggregator, gboolean timeout)
       gst_flv_mux_push (mux, buf);
     mux->new_metadata = FALSE;
   }
-
   if (best) {
     best->dts =
         gst_flv_mux_segment_to_running_time (&GST_AGGREGATOR_PAD
